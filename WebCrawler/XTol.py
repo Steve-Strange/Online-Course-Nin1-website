669fca268b0b127696f1319b7736656fdbbfe737
Sorcerer Inferior <2694630726@qq.com>
2023-07-26 16:45:18 +0800

    XTol debug

--- WebCrawler/XTol.py
+++ WebCrawler/XTol.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 def XTol(keyword, key):
 
@@ -12,7 +13,8 @@ def XTol(keyword, key):
 
     js = "window.open('{}','_blank');"
     chrome_options = Options()
-    chrome_options.add_argument('headless')
+    # chrome_options.add_argument('headless')
+    chrome_options.page_load_strategy = 'eager'
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(options=chrome_options)
 
@@ -30,27 +32,34 @@ def XTol(keyword, key):
             
             search_url = "https://www.xuetangx.com/search?query=" + keyword + "&classify=&type=&status=2&ss=manual_search&page=" + str(i)
             driver.get(search_url)
-            time.sleep(0.1)
-
+            time.sleep(0.6)
+            
             click_places = driver.find_elements(By.CLASS_NAME, "resultListCon")
+            time.sleep(0.1)
             click_place = click_places[j]
+            
+            covers = driver.find_elements(By.CLASS_NAME, "leftImg")
+            time.sleep(0.1)
+            cover = covers[j].find_elements(By.TAG_NAME, "img")[-1].get_attribute("src")      
             ActionChains(driver).move_to_element(click_place).click().perform()
-            time.sleep(0.8)
+            time.sleep(0.1)
             href = driver.current_url
             html_class = driver.page_source
             soup_class = BeautifulSoup(html_class, "lxml")
+            if(href.find("training") != -1):
+                continue
             print(href)
-            
-            name = soup_class.find(class_="title f32 c_f").get_text()     
-            detail = soup_class.find(class_="f14 c_6 lh23").get_text()
-            cover = soup_class.find(class_="trailer_cover").get('src')
-            play_num = int(soup_class.find(class_="entry-count").get_text().split(' ')[0][:-4])
-            
-            time_start = soup_class.find(class_="list list1").get_text().replace('\t', '').replace('\n', '').strip()[-10:]  
+            try:
+                name = soup_class.find(class_="title f32 c_f").get_text()     
+                detail = soup_class.find(class_="f14 c_6 lh23").get_text()
+                play_num = int(soup_class.find(class_="entry-count").get_text().split(' ')[0][:-4])
+                
+                time_start = soup_class.find(class_="list list1").get_text().replace('\t', '').replace('\n', '').strip()[-10:]
+            except Exception:
+                continue
             
             url_list.append([href, name, cover, detail, play_num, comments_num, score, time_start, time_span])
             
-    driver.quit()
 
     print("finish scrapping")
 
@@ -73,6 +82,5 @@ def XTol(keyword, key):
 
     return url_list
 
-if __name__ == "__main__":
-    final_list = XTol(input(), input())
-    print(final_list)
+final_list = XTol(input(), input())
+print(final_list)
