159ad08704bf3eb7ce6c7d92aabe9a33f8c9e124
Sorcerer Inferior <2694630726@qq.com>
2023-07-28 00:33:47 +0800

    iCourse debug...

--- WebCrawler/test_all.py
+++ WebCrawler/test_all.py
@@ -12,20 +12,23 @@ keyword = input()
 key = input()
 
 def main():
-    print(Bilibili(keyword,key))
-    print(Chinaooc(keyword, key))
-    print(CNMOOC(keyword, key))
-    print(HDXol(keyword, key))
-    print(iCourse(keyword, key))
-    print(iMOOC(keyword, key))
-    print(NetEase(keyword, key))
-    print(XTol(keyword, key))
-
-    print("success")
+    with open ("final_output.txt", 'a', encoding='utf-8') as f:
+        f.write(keyword)
+        f.write(key)
+        f.write(str(Bilibili(keyword,key)))
+        f.write(str(Chinaooc(keyword, key)))
+        f.write(str(CNMOOC(keyword, key)))
+        f.write(str(HDXol(keyword, key)))
+        f.write(str(iCourse(keyword, key)))
+        f.write(str(iMOOC(keyword, key)))
+        f.write(str(NetEase(keyword, key)))
+        f.write(str(XTol(keyword, key)))
+        f.write("success")
     
 if __name__ == "__main__":
     time_start = time.time() #开始计时
     main()
+    print("success")
     time_end = time.time()    #结束计时
     time_c= time_end - time_start   #运行所花时间
     print('time cost', time_c, 's')
